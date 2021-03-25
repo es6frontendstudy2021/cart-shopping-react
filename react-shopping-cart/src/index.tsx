93760b57bc5237a1d91d241300732e384be84347
류길수 <rlftn510@naver.com>
2021-03-25 11:39:52 +0900

    Revert "mobx 로의 스토어 관리"
    This reverts commit aefaa1c77bd8fa4170dc8ef6bbddd64b2f4f57ae.

--- react-shopping-cart/src/index.tsx
+++ react-shopping-cart/src/index.tsx
@@ -1,21 +1,13 @@
 import React from 'react';
-// import DevTools from 'mobx-react-devtools';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { Provider } from 'mobx-react'
-import { productStore } from './stores/ProductStore'
 
 const client = new QueryClient();
 
 ReactDOM.render(
-  <Provider store={productStore}>
+  <QueryClientProvider client={client}>
     <App />
-    {/* <DevTools /> */}
-  </Provider>,
-  
-  // <QueryClientProvider client={client}>
-  //   <App />
-  // </QueryClientProvider>,
+  </QueryClientProvider>,
   document.getElementById('root')
 );
