65d14e8397b3ffea609d7843f36b34c0c03ddc42
류길수 <rlftn510@naver.com>
2021-04-02 10:47:47 +0900

    css 수정

--- antd-clone/src/App.js
+++ antd-clone/src/App.js
@@ -1,19 +1,51 @@
 // import { Button } from './styles/Button'
-import { Route } from 'react-router-dom'
+import { Route, NavLink } from 'react-router-dom'
 import ButtonGroup from './containers/Button'
 import GlobalStyles from './components/GlobalStyles'
 import Buttons from './pages/ButtonPage'
 import ModalPage from './pages/ModalPage'
+import styled from 'styled-components'
+import './App.css'
 
+const CustomNav = styled.ul`
+  overflow: hidden;
+  > li {
+    float:left;
+    a {
+      display:block;
+      padding: 6px 10px;
+      font-size:18px;
+    }
+  }
+  .active-item{
+    background-color: #ff6000;
+    color: #fff;
+  }
+`
 
 function App() {
   return (
     <div className="App">
       <GlobalStyles></GlobalStyles>
-      <Route exact path="/" component={Buttons} />
-      <Route exact path="/button" component={Buttons} />
-      <Route exact path="/modal" component={ModalPage} />
-      {/* <ButtonGroup></ButtonGroup> */}
+      <CustomNav className="left-nav">
+        <li>
+        <NavLink to="/button" activeClassName="active-item">
+          Button
+        </NavLink>
+        </li>
+        <li>
+        <NavLink to="/modal"activeClassName="active-item">
+          Modal
+        </NavLink>
+        </li>
+      </CustomNav>
+      
+      <div className="app-container">
+        <Route exact path="/" component={Buttons} />
+        <Route exact path="/button" component={Buttons} />
+        <Route exact path="/modal" component={ModalPage} />
+        {/* <ButtonGroup></ButtonGroup> */}
+      </div>
     </div>
   );
 }
